cfc29929d6b7d3156aaa5a149feaf6b4d04ed252
百濑美月 <15230787+MomoseMitsuki@user.noreply.gitee.com>
2026-04-18 23:57:02 +0800

    feat(monitor-react): 处理react的路由跳转和错误监控

---
+++ packages/monitor-react/src/hooks/useRouterChange.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+import { lazyReportCache } from "@mitsuki-monitor-sdk/core";
+
+export const useRouterChangeReport = () => {
+	const location = useLocation();
+	const [fromPath, setFromPath] = useState<string>("");
+	const params = useParams();
+	const [search] = useSearchParams();
+	let pageViewStartTime = Date.now();
+
+	const formatParams = normalizeParams(params);
+	const formatQuery = normalizeQuery(search);
+
+	useEffect(() => {
+		const handleRouteChange = () => {
+			if (fromPath) {
+				const stayTime = Date.now() - pageViewStartTime;
+				pageViewStartTime = Date.now();
+				lazyReportCache("behavior", {
+					subType: "reactRouterChange",
+					params: formatParams,
+					query: formatQuery,
+					name: location.pathname,
+					from: fromPath,
+					to: location.pathname,
+					stayTime
+				});
+			}
+			setFromPath(location.pathname);
+		};
+		handleRouteChange();
+	}, [location, fromPath]);
+};
+
+export default useRouterChangeReport;
+
+function normalizeParams(params: Record<string, any>) {
+	return Object.fromEntries(Object.entries(params).map(([k, v]) => [k, v == null ? "" : v]));
+}
+
+function normalizeQuery(query: Record<string, any>) {
+	return Object.fromEntries(Object.entries(query).map(([k, v]) => [k, v == null ? null : v]));
+}
