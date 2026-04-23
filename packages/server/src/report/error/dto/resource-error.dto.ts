ad0a67886f5b1dbf0cc151b2cc857e3aa35b13bc
百濑美月 <15230787+MomoseMitsuki@user.noreply.gitee.com>
2026-04-23 22:29:59 +0800

    refactor(root): 移除client/server项目,单独创建monorepo工程项目

--- packages/server/src/report/error/dto/resource-error.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsString, IsNotEmpty } from "class-validator";
-import { BaseErrorDto } from "./base-error.dto";
-
-export class ResourceErrorDto extends BaseErrorDto {
-	@IsNotEmpty()
-	@IsString()
-	tagName!: string;
-}
--- packages/server/src/schema/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./apiLog.schema";
-export * from "./behavior.schema";
-export * from "./error.schema";
-export * from "./performance.schema";
-export * from "./userAction.schema";
